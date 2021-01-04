f981e22af85db659bfb44d2c96c140d256930ae2
kiwan kim <dkwanm1@snu.ac.kr>
2021-01-04 21:23:21 +0900

    font 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,29 @@
 import { AppProps } from 'next/app'
-import { Reset } from 'styled-reset'
+import reset from 'styled-reset'
 import Header from '../components/Header'
 import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  body {
+    font-family: SpoqaHanSansNeo;
+  }
+`
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css' rel='stylesheet' type='text/css' />
-        <title>
-          와플 홈피
-        </title>
+        <link
+          href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <title>와플 홈피</title>
       </Head>
-      <Reset />
+      <GlobalStyle />
       <Header />
       <Component {...pageProps} />
     </>
