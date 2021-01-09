1cb1f243dc463ee3887802cc43daed9e709774a5
kiwan kim <dkwanm1@snu.ac.kr>
2021-01-09 19:24:36 +0900

    fix order

--- components/common/index.tsx
+++ components/common/index.tsx
@@ -1,3 +1,4 @@
 export { default as DownloadLink } from './DownloadLink'
-export { default as SectionDivider } from './SectionDivider'
 export { default as SectionTitle } from './SectionTitle'
+export { default as SectionDivider } from './SectionDivider'
+
