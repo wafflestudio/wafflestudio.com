df785c84da4904b1a727e2f876200b39e0afb771
29din <rbwls5567@wafflestudio.com>
2021-02-08 16:21:37 +0900

    반응형 모바일 페이지 1차

--- components/Common/SectionDivider.tsx
+++ components/Common/SectionDivider.tsx
@@ -4,10 +4,10 @@ import { Colors } from 'util/constant'
 const SectionDivider = styled.div`
   width: 680px; 
   height: 6px;
-  background: ${Colors.backDark};
   border-radius: 25px;
   margin-top: 100px;
   margin-bottom: 100px;
+  background: ${Colors.backDark};
 `
 
 export default SectionDivider
