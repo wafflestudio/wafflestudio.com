8be0809c30c1f1a1dfe47beef7fbe17caf39db9a
ars-ki-00 <dkwanm1@snu.ac.kr>
2021-08-08 17:57:32 +0900

    common divider 두께 수정

--- components/Common/SectionDivider.tsx
+++ components/Common/SectionDivider.tsx
@@ -2,10 +2,9 @@ import styled from 'styled-components'
 import { Colors } from 'util/constant'
 
 const SectionDivider = styled.div`
-  width: 680px; 
-  height: 6px;
+  width: 780px;
+  height: 1px;
   background: ${Colors.backDark};
-  border-radius: 25px;
   margin-top: 100px;
   margin-bottom: 100px;
 
